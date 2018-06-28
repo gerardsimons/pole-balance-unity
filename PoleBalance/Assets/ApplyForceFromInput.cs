f549d7de9fc25b39361ba2b69fad418f6b2eb9f6
Gerard Simons <sjrasimons@gmail.com>
2018-06-28 23:06:12 +0100

    Initial commit

---
+++ PoleBalance/Assets/ApplyForceFromInput.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplyForceFromInput : MonoBehaviour {
+
+    public Vector3 forceVector;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("space"))
+        {
+            print("space key was pressed");
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            print("Left arrow pressed!");
+            gameObject.GetComponent<Rigidbody>().AddForce(forceVector);
+
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            print("Right arrow pressed!");
+            gameObject.GetComponent<Rigidbody>().AddForce(-forceVector);
+        }
+    }
+}
